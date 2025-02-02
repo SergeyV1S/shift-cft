ea18d56a7892f54c1c8cc0c30b7c15f84efdb8f6
Sergey Yatsuk <v1s.coop@yandex.ru>
2025-02-02 12:59:15 +0500

    add pwa feat

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,41 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Shift Delivery",
+        short_name: "SD",
+        description: "Shift Delivery",
+        theme_color: "#ffffff",
+        lang: "ru",
+        start_url: "/",
+        icons: [
+          {
+            purpose: "maskable",
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "/icon512_maskable.png",
+            type: "image/png"
+          },
+          { purpose: "any", sizes: "512x512", src: "/icon512_rounded.png", type: "image/png" }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "src/app"),
